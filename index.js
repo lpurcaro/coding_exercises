b4954f866f34c3fc54e509a68fdfc9888cfb59aa
Lucila Purcaro <purcarolucila@gmail.com>
2023-10-20 14:35:15 -0300

    implement url module for parseUrl

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { animals: { Lion, Tiger } } = require('./classes/Zoo');
 const { parseUrl } = require('./utilities/helpers');
 
+
 const lion = new Lion();
 const tiger = new Tiger();
 
-console.log(lion.speak("I'm a lion"));
-console.log(tiger.speak("Lions suck"));
+// console.log(lion.speak("I'm a lion"));
+// console.log(tiger.speak("Lions suck"));
 
 
 const parsedUrl = parseUrl('/:version/api/:collection/:id', '/6/api/listings/3?sort=desc&limit=10');
--- utilities/helpers.js
+++ utilities/helpers.js
@@ -1,3 +1,5 @@
+const url = require('url');
+
 function getDiffInSeconds (dateA, dateB) {
     const millis = dateB - dateA;
     return Math.floor(millis / 1000) ;
@@ -8,26 +10,20 @@ function parseValue (value) {
 }
 
 function parseUrl (format, instance) {
-    // get keys for response
+    const { query, pathname } = url.parse(instance, true);
+    const [, ...params] = pathname.split(/\//);
     const [, ...variables] = format.split(/\//);
-    // get values for response (there's also query params that will be handled later)
-    const [, ...params] = instance.split(/\/|\?|&/);
 
-    const mappedUrl = params.reduce((mappedObj, param, idx) => {
+    const mappedUrl = variables.reduce((mappedObj, variable, idx) => {
         const newParam = {};
-
-        if (!variables[idx]) {
-            // if variable does't exist at that idx it means we are iterating over a query param
-            const [urlQueryKey, urlQueryValue] = param.split(/=/);
-            newParam[urlQueryKey] = parseValue(urlQueryValue);
-        } else if (variables[idx].startsWith(':')) {
-            // if variable exists at that idx we have to evaluate if is a parameter variable (begin with a colon)
-            const [, urlParamKey] = variables[idx].split(/:/);
-            newParam[urlParamKey] = parseValue(param);
+        
+        if (variable.startsWith(':')) {
+            const [, urlParamKey] = variable.split(/:/);
+            newParam[urlParamKey] = parseValue(params[idx]);
         }
 
         return { ...mappedObj, ...newParam};
-    }, {});
+    }, query);
 
     return mappedUrl;
 
